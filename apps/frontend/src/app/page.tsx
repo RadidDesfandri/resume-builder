f5ac0397e8f76bc69c5fc42da31a2de24b4662ff
RadidDesafandri <radietdesfandri@gmail.com>
2025-02-19 13:44:22 +0700

    new branch development

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-full w-full items-center justify-center bg-black text-white">
-      HALLO GUYS
+      HALLO DEVELOPMENTS
     </div>
   );
 }
