4a0d54010fb31bcde052f8fa577d989661c733fc
RadidDesafandri <radietdesfandri@gmail.com>
2025-03-01 15:09:47 +0700

    delete folder dashboard and delete unused files

--- apps/frontend/src/app/dashboard/explore/page.tsx
+++
@@ -1,9 +0,0 @@
-const Explore = () => {
-  return (
-    <div className="flex h-full w-full items-center justify-center text-3xl">
-      Coming soon
-    </div>
-  );
-};
-
-export default Explore;
